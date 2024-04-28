d7358725d69c60ba737acc9138af19fac9a457a4
Setthapon Thadisakun <steam.reaw1@gmail.com>
2024-04-28 14:33:33 +0700

    delete all file

--- swagger_server/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# flake8: noqa
-# import models into model package
-from swagger_server.models.all import All
-from swagger_server.models.aqi import Aqi
-from swagger_server.models.humidity import Humidity
-from swagger_server.models.pm10 import Pm10
-from swagger_server.models.pm25 import Pm25
-from swagger_server.models.temperature import Temperature
